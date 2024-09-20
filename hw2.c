2696b945b577b933f0a98067ca42b25b2fefb6df
An1207 <seho127@naver.com>
2024-09-20 16:53:02 +0900

    add c File [hw2.c]

---
+++ hw2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	double km, miles;
+	printf("Please enter klometers : ");
+	scanf("%lf", &km);
+
+	miles = km / 1.609;
+
+	printf("%.1f km is equal to %.1f miles", km, miles);
+
+	return 0;
+}
